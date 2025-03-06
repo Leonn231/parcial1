1de0b8d65b7c188b12d4f49f7789788b08c9d757
Miguel Bedoya <158485910+Leonn231@users.noreply.github.com>
2025-03-06 09:16:47 -0500

    Delete Parcial1/Parcial directory

--- Parcial1/Parcial/src/main/java/com/registrorecetas/repository/RecetaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cuervo.Parcial.repository;
-
-import com.cuervo.Parcial.model.Evento;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventoRepository extends CrudRepository<Evento,  Long> {}
